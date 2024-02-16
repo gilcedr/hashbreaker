fd23696726fbe8169ae31d6317011de664bc8d46
gilcedr <137206047+gilcedr@users.noreply.github.com>
2024-02-16 15:14:54 +0000

    Add files via upload

---
+++ gil_design.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int gil_design() {
+    printf("\n");
+    printf("\n");
+    printf("\033[32m");  // Changer la couleur du texte à vert
+    printf("                                                                                                           \n");
+    printf("             88  88     88                                               88                                \n");
+    printf("             \"\"  88     88                                               88                                \n");
+    printf("                 88     88                                               88                                \n");
+    printf(" ,adPPYb,d8  88  88     88,dPPYba,   8b,dPPYba,   ,adPPYba,  ,adPPYYba,  88   ,d8   ,adPPYba,  8b,dPPYba,  \n");
+    printf("a8\"    `Y88  88  88     88P'    \"8a  88P'   \"Y8  a8P_____88  \"\"     `Y8  88 ,a8\"   a8P_____88  88P'   \"Y8  \n");
+    printf("8b       88  88  88     88       d8  88          8PP\"\"\"\"\"\"\"  ,adPPPPP88  8888[     8PP\"\"\"\"\"\"\"  88          \n");
+    printf("\"8a,   ,d88  88  88     88b,   ,a8\"  88          \"8b,   ,aa  88,    ,88  88`\"Yba,  \"8b,   ,aa  88          \n");
+    printf(" `\"YbbdP\"Y8  88  88     8Y\"Ybbd8\"'   88           `\"Ybbd8\"'  `\"8bbdP\"Y8  88   `Y8a  `\"Ybbd8\"'  88          \n");
+    printf(" aa,    ,88                                                                                                \n");
+    printf("  \"Y8bbdP\"                                                                                                 \n");
+    printf("\033[0m");  // Réinitialiser la couleur du texte
+
+    printf("\n");
+    printf("\n");
+    return 0 ;
+}
+
